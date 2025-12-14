edfaaa3cf83837de3b2e1dcf5921358069d4e433
tronghoa <duongtronghoa05@gmail.com>
2025-12-14 22:14:05 +0700

    Feat: Setup API service client

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+const BASE_URL = "/api";
+const APP_TOKEN =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjIzXzMxIiwicm9sZSI6InVzZXIiLCJhcGlfYWNjZXNzIjp0cnVlLCJpYXQiOjE3NjUzNjE3NjgsImV4cCI6MTc3MDU0NTc2OH0.O4I48nov3NLaKDSBhrPe9rKZtNs9q2Tkv4yK0uMthoo";
+
+async function fetchClient(endpoint, options = {}) {
+    const token = localStorage.getItem("accessToken");
+    const headers = {
+        "Content-Type": "application/json",
+        "x-app-token": APP_TOKEN,
+        ...options.headers,
+    };
+
+    if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+        ...options,
+        headers,
+    });
+
+    return handleResponse(response);
+}
