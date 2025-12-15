dd30d5813da340ebaae551b3f20de982d25a0b54
tronghoa <duongtronghoa05@gmail.com>
2025-12-15 15:48:36 +0700

    Feat : Add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Outlet,
+} from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-function App() {
+import SearchResults from "./pages/SearchResults";
+import MovieDetail from "./pages/MovieDetail";
+import PersonDetail from "./pages/PersonDetail";
+
+import Favorites from "./pages/Favorites";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Layout() {
     return (
-        <BrowserRouter>
-            <AuthProvider>
-                <div className="min-h-screen bg-background text-foreground flex flex-col">
-                    <Header />
-                    <main className="flex-1">
-                        <Routes>
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route
-                                path="/"
-                                element={
-                                    <div className="p-8 text-center">
-                                        Home Page Coming Soon
-                                    </div>
-                                }
-                            />
-                        </Routes>
-                    </main>
-                    <Footer />
-                </div>
-            </AuthProvider>
-        </BrowserRouter>
+        <div className="min-h-screen flex flex-col font-sans text-foreground">
+            <Header />
+            <main className="flex-1 w-full max-w-[1200px] mx-auto px-4 py-6">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     );
 }
+
+function App() {
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/search" element={<SearchResults />} />
+                        <Route path="/movie/:id" element={<MovieDetail />} />
+                        <Route path="/person/:id" element={<PersonDetail />} />
+                        <Route
+                            path="/favorites"
+                            element={
+                                <ProtectedRoute>
+                                    <Favorites />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/profile"
+                            element={
+                                <ProtectedRoute>
+                                    <Profile />
+                                </ProtectedRoute>
+                            }
+                        />
+                        {/* Add other protected routes here */}
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </Router>
+    );
+}
+
 export default App;
